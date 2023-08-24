055a1acd73c6f6d5779fb5b8776ea928c1d4aa01
Lukasz Kryskiewicz <lukasz.kryskiewicz@gmail.com>
2023-08-24 14:18:44 +0200

    implement esbuild.wasm and custom plugin to enable pkg fetching

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useRef, useState } from 'react';
+import * as esbuild from 'esbuild-wasm';
+import { unpkgPathPlugin } from './plugins/unpkg-path-plugin';
 
-function App() {
+const App = () => {
+  const [input, setInput] = useState<string>('');
+  const [code, setCode] = useState<string>('');
+  const ref = useRef<any>();
+
+  const startService = async () => {
+    ref.current = await esbuild.startService({
+      worker: true,
+      wasmURL: '/esbuild.wasm',
+    });
+  };
+
+  useEffect(() => {
+    startService();
+  }, []);
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (!ref.current) return;
+
+    /*    const result = await ref.current.transform(input, {
+      loader: 'jsx',
+      target: 'es2015',
+    }); */
+
+    const result = await ref.current.build({
+      entryPoints: ['index.js'],
+      bundle: true,
+      write: false,
+      plugins: [unpkgPathPlugin()],
+      define: {
+        'process.env.NODE_ENV': '"production"',
+        global: 'window',
+      },
+    });
+    console.log(result);
+    setCode(result.outputFiles[0].text);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <form onSubmit={handleSubmit}>
+        <textarea
+          rows={10}
+          cols={100}
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
+        <div>
+          <button type="submit">Submit</button>
+        </div>
+      </form>
+      <pre>{code}</pre>
     </div>
   );
-}
+};
 
 export default App;
